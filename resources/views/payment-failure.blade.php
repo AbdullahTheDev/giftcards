0318fe346a61f447c5fee307a0605beaec8ee490
Abdullah <abdullahwaseem.4401@gmail.com>
2024-09-09 22:45:02 +0500

    wokting

--- resources/views/payment-failure.blade.php
+++ resources/views/payment-failure.blade.php
@@ -1,21 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    
+    <link rel="stylesheet" href="{{ asset('assets/assets_admin/css/style.css') }}" />
+    <!-- fontawsome cdn  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Payment Failed</title>
 </head>
+
 <body>
-    <h1>Payment Failed</h1>
 
-    @if(session('error'))
-        <div class="alert alert-danger">
-            {{ session('error') }}
+
+    <div class="pay-card-failed">
+        <div class="card-iner-failed">
+            <i class="fa-solid fa-circle-exclamation"></i>
         </div>
-    @endif
+        <h1>Payment Failed</h1>
+        <p>There was an issue processing your payment. Please try again later or contact support</p>
+        <a href="{{ route('home') }}">Go to Home Page</a>
 
-    
-    <p>There was an issue processing your payment. Please try again later or contact support.</p>
-    <!-- You can provide troubleshooting tips or contact information for support here. -->
+        <p>
+            @if (session('error'))
+                <div class="alert alert-danger">
+                    {{ session('error') }}
+                </div>
+            @endif
+        </p>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
+
+
+
+<p>.</p>
--- resources/views/payment-success.blade.php
+++ resources/views/payment-success.blade.php
@@ -9,12 +9,12 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="{{ asset('assets/assets_admin/css/style.css') }}" />
     <!-- fontawsome cdn  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
         integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Login</title>
+    <title>Gift</title>
 </head>
 
 <body>
@@ -27,12 +27,6 @@
         <h1>Gift Sent Successfully</h1>
         <p>Your gift has been sent successfully</p>
         <a href="{{ route('home') }}">Go to Home Page</a>
-
-        @if (session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
