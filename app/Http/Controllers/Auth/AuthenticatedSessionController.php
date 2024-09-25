6fe53a207e05c272b2fa904bf107991ce4c89e6a
Abdullah <abdullahwaseem.4401@gmail.com>
2024-09-25 22:45:03 +0500

    gm

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -102,30 +102,4 @@ class AuthenticatedSessionController extends Controller
         Mail::to($user->email)->send(new \App\Mail\VerificationCode($code));
     }
 
-
-    public function verify(Request $request)
-    {
-        $request->validate([
-            'code' => 'required|string|size:6',
-        ]);
-
-        // Check the entered code against the stored session code
-        if ($request->code === session('verification_code')) {
-            // Log the user in
-            Auth::loginUsingId(session('user_id'));
-
-            // Clear the session values
-            session()->forget(['verification_code', 'user_id']);
-
-            if (Auth::user()->role == 'admin') {
-                return redirect()->intended(RouteServiceProvider::ADMIN);
-            }
-            // Redirect to intended route
-            return redirect()->intended(RouteServiceProvider::HOME); // Change as needed
-        }
-
-        // If the code is invalid, redirect back with an error
-        return back()->withErrors(['code' => 'The verification code is invalid.']);
-    }
-
 }
